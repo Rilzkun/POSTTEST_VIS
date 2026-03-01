b8db01ea0b1615f53a6172c5f655ef8d74bd4872
Rilzkun <ranggalawe58191@gmail.com>
2026-03-01 19:01:42 +0800

    done

--- POSTTEST_1/Form1.Designer.vb
+++ POSTTEST_1/Form1.Designer.vb
@@ -22,29 +22,98 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Button1 = New Button()
+        btnTambah = New Button()
+        btnReset = New Button()
+        txtIPSemester = New TextBox()
+        txtIPKumulatif = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        lblStatus = New Label()
         SuspendLayout()
         ' 
-        ' Button1
+        ' btnTambah
         ' 
-        Button1.Location = New Point(420, 83)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(94, 29)
-        Button1.TabIndex = 0
-        Button1.Text = "Button1"
-        Button1.UseVisualStyleBackColor = True
+        btnTambah.Location = New Point(274, 148)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(94, 29)
+        btnTambah.TabIndex = 0
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.Location = New Point(374, 148)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(94, 29)
+        btnReset.TabIndex = 1
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' txtIPSemester
+        ' 
+        txtIPSemester.Location = New Point(274, 115)
+        txtIPSemester.Name = "txtIPSemester"
+        txtIPSemester.Size = New Size(194, 27)
+        txtIPSemester.TabIndex = 2
+        ' 
+        ' txtIPKumulatif
+        ' 
+        txtIPKumulatif.Location = New Point(274, 193)
+        txtIPKumulatif.Name = "txtIPKumulatif"
+        txtIPKumulatif.Size = New Size(194, 27)
+        txtIPKumulatif.TabIndex = 3
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(177, 115)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(86, 20)
+        Label1.TabIndex = 4
+        Label1.Text = "IP Semester"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(177, 193)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(89, 20)
+        Label2.TabIndex = 5
+        Label2.Text = "IP Kumulatif"
+        ' 
+        ' lblStatus
+        ' 
+        lblStatus.AutoSize = True
+        lblStatus.Location = New Point(339, 232)
+        lblStatus.Name = "lblStatus"
+        lblStatus.Size = New Size(53, 20)
+        lblStatus.TabIndex = 6
+        lblStatus.Text = "Label3"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
-        Controls.Add(Button1)
+        Controls.Add(lblStatus)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(txtIPKumulatif)
+        Controls.Add(txtIPSemester)
+        Controls.Add(btnReset)
+        Controls.Add(btnTambah)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents txtIPSemester As TextBox
+    Friend WithEvents txtIPKumulatif As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblStatus As Label
 
 End Class
--- POSTTEST_1/Form1.vb
+++ POSTTEST_1/Form1.vb
@@ -1,3 +1,43 @@
 ﻿Public Class Form1
+    Dim totalIP As Double = 0
+    Dim jumlahSemester As Integer = 0
 
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        If String.IsNullOrWhiteSpace(txtIPSemester.Text) Then
+            MsgBox("Silakan isi IP Semester terlebih dahulu!", MsgBoxStyle.Exclamation)
+            Exit Sub
+        End If
+
+        Dim ipInput As Double = Val(txtIPSemester.Text.Replace(",", "."))
+
+        totalIP += ipInput
+        jumlahSemester += 1
+
+        Dim ipk As Double = totalIP / jumlahSemester
+        txtIPKumulatif.Text = Format(ipk, "0.00")
+
+        If ipk >= 3.01 Then
+            lblStatus.Text = "Sangat Memuaskan"
+        ElseIf ipk >= 2.76 Then
+            lblStatus.Text = "Memuaskan"
+        ElseIf ipk >= 2.0 Then
+            lblStatus.Text = "Cukup"
+        Else
+            lblStatus.Text = "Di bawah standar"
+        End If
+
+        txtIPSemester.Clear()
+        txtIPSemester.Focus()
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        totalIP = 0
+        jumlahSemester = 0
+
+        txtIPSemester.Clear()
+        txtIPKumulatif.Clear()
+        lblStatus.Text = "Status"
+
+        txtIPSemester.Focus()
+    End Sub
 End Class
